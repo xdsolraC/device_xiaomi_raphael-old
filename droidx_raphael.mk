8221bb1338050cd11ad70ea531940b0f24fcb26a
DanipunK1 <pegate.eltrueco@gmail.com>
2023-01-15 23:54:51 +0100

    raphael: Include ViPER4Android Redesigned to ROM git clone https://github.com/DanipunK1/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX

--- droidx_raphael.mk
+++ droidx_raphael.mk
@@ -15,6 +15,9 @@
 # Inherit from raphael device.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
+# ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Inherit some common DroidxUI stuff.
 $(call inherit-product, vendor/droidx/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
