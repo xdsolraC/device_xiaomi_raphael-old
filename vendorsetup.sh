e50a9be7d2bc6055be4ea8a068abe3174d136c2f
xdsolraC <xds0lr4c@gmail.com>
2023-05-20 13:20:13 -0400

    clone ViPER4AndroidFX

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/xdsolraC/hardware_qcom_display.git hardware/qcom-ca
 git clone https://github.com/DanipunK1/vendor_xiaomi_raphael -b ch13 vendor/xiaomi/raphael --depth=1
 git clone https://github.com/onettboots/bool-x_xiaomi_raphael -b saaya2 kernel/xiaomi/raphael --depth=1
 git clone https://gitlab.com/onettboots/boolx-clang.git -b Clang-16.0_x86 prebuilts/clang/host/linux-x86/boolx-clang --depth=1
+git clone https://github.com/xdsolraC/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
